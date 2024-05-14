c852c0793accb43c9b493e5582471d9b950aad16
williansql <willslipknotsp@gmail.com>
2024-05-14 19:12:39 -0300

    Listagem de categorias

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,4 +2,10 @@ import { Routes } from '@angular/router';
 
 
 export const routes: Routes = [
+    {
+        path: '',
+        loadComponent: () =>
+            import('./items/category/category.component')
+        .then((m) => m.CategoryComponent),
+    }
 ];
